b2a6d194cbb8417ccba2e3119a8461b460ca95c1
upendrashrestha10 <shresthaupendra1234@gmail.com>
2025-08-26 14:17:37 +0545

    Inital commit

---
+++ Task/even_odd.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main(){
+    stdout.write("Enter an interger: ");
+    int a =int.parse(stdin.readLineSync()!);
+    if (a%2 == 0){
+        print("Given number is Even");
+    }
+    else{
+        print("Given number is odd");
+    }
+
+}
---
+++ Task/simple_calc(if_Else).dart
@@ -0,0 +1,58 @@
+
+import 'dart:io';
+
+void main(){
+  stdout.write("Enter first number: ");
+  int a = int.parse(stdin.readLineSync()!);
+  stdout.write("Enter 2nd number: ");
+  int b = int.parse(stdin.readLineSync()!);
+  stdout.write(" 1.Add\n 2.Subtract\n 3.Division\n 4.Multiply\n Enter your choice: ");
+  int choice  = int.parse(stdin.readLineSync()!);
+
+// //using switch()case.
+
+  // switch(choice) {
+  //   case 1:
+  //     print('Addition: ${a+b} ');
+  //     break;
+    
+  //   case 2:
+  //     print('Subtraction: ${a-b}');
+  //     break;
+    
+  //   case 3:
+  //     print('Division: ${a/b}');
+  //     break;
+
+  //   case 4:
+  //     print('Multiplication: ${a*b}');
+  //     break;
+
+  //   default:
+  //     print("Invalid choice ....!");
+
+  // }
+
+
+//using if else.
+
+  if(choice == 1){
+    print("Addition: ${a+b}");
+  }
+
+  else if(choice == 2){
+    print("Subtraction: ${a-b}");
+  }
+  else if(choice == 3){
+    print("Division: ${a/b}");
+  }
+  else if(choice == 4){
+    print("Multiplication: ${a*b}");
+  }
+
+  else{
+    print("invalid choice.....!");
+  }
+
+    
+}
